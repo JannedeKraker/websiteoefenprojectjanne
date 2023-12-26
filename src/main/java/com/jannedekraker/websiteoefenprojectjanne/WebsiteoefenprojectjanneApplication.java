1a3914930cac0d9179f6b03fa5cef55ab501c32c
ennaj_janne <ennaj_janne@hotmail.com>
2023-12-26 14:59:30 +0100

    Initial commit

---
+++ src/main/java/com/jannedekraker/websiteoefenprojectjanne/WebsiteoefenprojectjanneApplication.java
@@ -0,0 +1,13 @@
+package com.jannedekraker.websiteoefenprojectjanne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteoefenprojectjanneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsiteoefenprojectjanneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jannedekraker/websiteoefenprojectjanne/WebsiteoefenprojectjanneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jannedekraker.websiteoefenprojectjanne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteoefenprojectjanneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
